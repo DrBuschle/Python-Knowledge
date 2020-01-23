b98d3edd705e3cc56e24d90edc1496a545252703
DrBuschle <dr.buschle@hotmail.com>
2020-01-23 17:24:11 +0100

    Newest_updates

---
+++ Pause_a_script/time.sleep.py
@@ -0,0 +1,12 @@
+#### Pause a script
+
+import time 
+
+time.sleep(1)	# pauses the script for a second
+
+
+############# alternative ########
+
+from time import sleep
+
+sleep(1)	 # pauses the script for a second
